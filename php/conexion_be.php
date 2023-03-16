62f4ad31f0ac2d8931f8128a108b7bbbf21cc6fc
Dani1410 <wolfchis0@gmail.com>
2023-03-16 17:57:38 -0600

    Agregamos conexion a server

--- php/conexion_be.php
+++ php/conexion_be.php
@@ -1,9 +1,9 @@
 <?php 
 
     $user = "localhost";
-    $nombre_usuario = "root";
-    $password = "";
-    $nombre_bd = "blog_db";
+    $nombre_usuario = "id20446774_dani";
+    $password = "EXw9(qp]_A-5BcTz";
+    $nombre_bd = "id20446774_blog";
 
     $conexion = mysqli_connect($user, $nombre_usuario, $password, $nombre_bd);
 
