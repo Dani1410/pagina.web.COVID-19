04c5a486d05cd9f69aad23f153e7463aa37f30c3
Dani1410 <wolfchis0@gmail.com>
2023-06-20 15:40:22 -0600

    Comprimimos los js

--- js/header.js
+++ js/header.js
@@ -1,6 +1,1 @@
-// Si el scroll es mayor a 620 se le agregara una clase a el header y en css se agregaran los estilos a esa clase
-
-window.addEventListener("scroll", function(){
-    var header = this.document.querySelector("header");
-    header.classList.toggle("abajo", this.window.scrollY>620);
-})
+window.addEventListener("scroll",function(){this.document.querySelector("header").classList.toggle("abajo",620<this.window.scrollY)});
